1f3253be0c57dd45b5feb1dce01e4da754ea9f8e
ahnjb <jesse890047@naver.com>
2025-05-13 21:36:16 +0900

    feat : 버튼 추가, 버튼 이미지 추가가

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+// 사이드 박스 제어
 function openSidebar() {
   document.getElementById("sidebar").classList.add("active");
 }
-
 function closeSidebar() {
   document.getElementById("sidebar").classList.remove("active");
 }
+
+// 모든 체크박스 선택
+document.addEventListener("DOMContentLoaded", function () {
+  const selectAllBtn = document.getElementById("selectAllBtn");
+  const deselectAllBtn = document.getElementById("deselectAllBtn");
+
+  selectAllBtn.addEventListener("click", function () {
+    const checkboxes = document.querySelectorAll(".itam");
+    checkboxes.forEach(cb => cb.checked = true);
+  });
+
+  deselectAllBtn.addEventListener("click", function () {
+    const checkboxes = document.querySelectorAll(".itam");
+    checkboxes.forEach(cb => cb.checked = false);
+  });
+});
